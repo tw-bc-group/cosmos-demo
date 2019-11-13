3a386ae2b4e2ce797e432e7f8d9aab7ac5711df0
Chaowei Zeng <sli616@163.com>
2019-11-13 14:33:30 +0800

    initial commit for Namespace.

---
+++ x/nameservice/alias.go
@@ -0,0 +1,33 @@
+package nameservice
+
+import (
+	"github.com/arthaszeng/nameservice/x/nameservice/keeper"
+	"github.com/arthaszeng/nameservice/x/nameservice/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	RouterKey  = types.RouterKey
+	StoreKey   = types.StoreKey
+)
+
+var (
+	NewKeeper        = keeper.NewKeeper
+	NewQuerier       = keeper.NewQuerier
+	NewMsgBuyName    = types.NewMsgBuyName
+	NewMsgSetName    = types.NewMsgSetName
+	NewMsgDeleteName = types.NewMsgDeleteName
+	NewWhois         = types.NewWhois
+	ModuleCdc        = types.ModuleCdc
+	RegisterCodec    = types.RegisterCodec
+)
+
+type (
+	Keeper          = keeper.Keeper
+	MsgSetName      = types.MsgSetName
+	MsgBuyName      = types.MsgBuyName
+	MsgDeleteName   = types.MsgDeleteName
+	QueryResResolve = types.QueryResResolve
+	QueryResNames   = types.QueryResNames
+	Whois           = types.Whois
+)
